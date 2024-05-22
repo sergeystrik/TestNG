df8b1eb0284a84357dbb3932eedfc81b93f3b574
sergeystrik <sergeystrik@gmail.com>
2024-05-21 20:05:38 -0400

    !commit

---
+++ src/test/java/class01/annotations.java
@@ -0,0 +1,19 @@
+package class01;
+
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class annotations {
+    @BeforeMethod
+    public void beforeMethod(){
+        System.out.println("i am before method");
+    }
+    @Test
+    public void ATestCase(){
+        System.out.println("i am test case A");
+    }
+    @Test
+    public void BTestCase(){
+        System.out.println("i am test case B");
+    }
+}
